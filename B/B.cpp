a839cc11b7d08d633280ad403541a3b6e56a5754
Nomad192 <verenya08@gmail.com>
2022-04-27 20:18:30 +0300

    add run.bat script

---
+++ B/B.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+
+int main()
+{
+	printf("Hi");
+
+	return 0;
+}
+
+
+/* 
+ * set 
+mkdir build & cd build & cmake -G "MinGW Makefiles" ..
+
+* build and run
+cmake --build . && (echo START & b)
+echo return code: %errorlevel%
+
+* or
+..\..\run b
+
+* move
+cd 2sem_AlgoLab_2\B\build
+*/
