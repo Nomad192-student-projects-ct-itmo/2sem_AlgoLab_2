d168178bdc6c200f8646f0210d7cb69a445eed2e
Nomad <verenya08@gmail.com>
2022-05-17 18:55:18 +0300

    Draft F v2

--- F/F.cpp
+++ F/F.cpp
@@ -3,8 +3,8 @@
 
 #define MAX(x, y) ((x > y) ? (x) : (y))
 
-#define TREE_TYPE int
-#define TREE_TYPE_SP "d"
+#define TREE_TYPE unsigned int
+#define TREE_TYPE_SP "u"
 
 #ifdef TEST
 	#include "C_Test.hpp"
@@ -17,14 +17,15 @@
 		{
 			T x;
 			signed char b = 0;
-			size_t deep = 1;
-			T sum;
+			size_t h = 1;
+			size_t deep;
+			T sum = 0;
 			Node *p;
 			Node *l = nullptr;
 			Node *r = nullptr;
 
-			explicit Node(T &x) : x(x), sum(x), p(nullptr) {}
-			Node(T &x, Node *p) : x(x), sum(x), p(p) {}
+			explicit Node(T &x) : x(x), p(nullptr), deep(1) {}
+			Node(T &x, Node *p) : x(x), p(p), deep(p->deep + 1) {}
 			~Node()
 			{
 				delete l;
@@ -45,21 +46,21 @@
 		T sum(T l, T r);
 
 		void full_print();
-		void full_print_stream(std::ostream &out);
+		void print_stream(std::ostream &out);
 
 	private:
 		static Node *search(Node *cur, T &x);
 		void del_node(Node *cur);
-		bool next_below(Node *cur, T x, T *res);
-		bool prev_below(Node *cur, T x, T *res);
+		bool next_below(Node *cur, T x, Node **res_node, T *res);
+		bool prev_below(Node *cur, T x, Node **res_node, T *res);
 
         void fix_balance_top(Node *cur);
 		void full_print_rq(Node *cur, size_t &ind);
-		void full_print_stream_rq(Node *cur, size_t &ind, std::ostream &out);
+		void print_stream_rq(Node *cur, size_t &ind, std::ostream &out);
 		bool insert_rq(Node *cur, T &x);
 		static void swap_val(Node *a, Node *b);
 		static void swap_childs(Node *cur);
-		static void restore_parent(Node *cur);
+		static void restore_childs(Node *cur);
 		static void balancing_node(Node *cur);
 		static void small_left_rotation(Node *cur);
 		static void large_left_rotation(Node *cur);
@@ -71,11 +72,39 @@
 
 AVL::T AVL::sum(T l, T r)
 {
+	if(!root) return 0;
+	if(l > r) return 0;
+
 	Node *l_node = search(root, l);
 	Node *r_node = search(root, r);
 
-	if(l_node->x != x) l_node = next_below();
-	if(r_node->x != x) r_node = prev_below();
+	//printf("a");
+	if(l_node->x != l) {
+		//printf("b");
+		T x; if(!next_below(root, l, &l_node, &x)) {return 0;} }	
+	if(r_node->x != r) {
+		//printf("c");
+		T x; if(!prev_below(root, r, &r_node, &x)) {return 0;} }
+
+	if(l_node == r_node) return l_node->x;
+
+	//printf("d");
+
+	T sum_l = l_node->sum;
+	T sum_r = r_node->sum;	
+
+	while(l_node->deep > r_node->deep) l_node = l_node->p;	
+	if(l_node == r_node) return l_node->x + l_node->l->sum + l_node->l->x - sum_l; 
+
+	while(l_node->deep  < r_node->deep) r_node = r_node->p;	
+	if(l_node == r_node) return l_node->x + l_node->r->sum + l_node->r->x - sum_r;
+
+	while(l_node != r_node)
+	{
+		l_node = l_node->p;
+		r_node = r_node->p;	
+	}
+	return l_node->x + l_node->sum - sum_l - sum_r;
 }
 
 AVL::Node *AVL::search(Node *cur, T &x)
@@ -100,7 +129,7 @@ bool AVL::next(T x, T *res)
 {
 	if(!root) return false;
 	Node *res_node = nullptr;
-	return next_rq(root, x, &res_node, res);
+	return next_below(root, x, &res_node, res);
 }
 bool AVL::next_below(Node *cur, T x, Node **res_node, T *res)
 {
@@ -120,7 +149,7 @@ bool AVL::next_below(Node *cur, T x, Node **res_node, T *res)
             *res = cur->x;
             *res_node = cur;
             if (!cur->l) {return true;}
-            else {next_rq(cur->l, x, res); return true;}
+            else {next_below(cur->l, x, res_node, res); return true;}
         }
         else if (x > cur->x) {if (!cur->r) {return false;} else cur = cur->r;}
     }
@@ -131,7 +160,7 @@ bool AVL::prev(T x, T *res)
 {
 	if(!root) return false;
 	Node *res_node = nullptr;
-	return prev_rq(root, x, &res_node, res);
+	return prev_below(root, x, &res_node, res);
 }
 bool AVL::prev_below(Node *cur, T x, Node **res_node, T *res)
 {
@@ -152,7 +181,7 @@ bool AVL::prev_below(Node *cur, T x, Node **res_node, T *res)
             *res = cur->x;
             *res_node = cur;
             if (!cur->r) {return true;}
-            else {prev_rq(cur->r, x, res); return true;}
+            else {prev_below(cur->r, x, res_node, res); return true;}
         }
     }
 }
@@ -260,7 +289,7 @@ void AVL::del_node(Node *cur)
 		if(p_leaf->l == leaf)	p_leaf->l = leaf->r; 
 		else  					p_leaf->r = leaf->r;
 
-		restore_parent(p_leaf);
+		restore_childs(p_leaf);
 
 		cur->x = leaf->x;
 		leaf->r = nullptr;
@@ -285,11 +314,11 @@ void AVL::full_print()
     full_print_rq(root, ind);
 	printf("%d\n", n);
 }
-void AVL::full_print_stream(std::ostream &out)
+void AVL::print_stream(std::ostream &out)
 {
 	out << n << '\n';
     size_t ind = 0;
-    full_print_stream_rq(root, ind, out);
+    print_stream_rq(root, ind, out);
 	out << n;
 }
 
@@ -303,7 +332,7 @@ void AVL::full_print_rq(Node *cur, size_t &ind)
     
     ind++;
 
-    printf("%-2zu - x=%-2" TREE_TYPE_SP " h=%-2zu b=%-2hhd -", ind, cur->x, cur->deep, cur->b);
+    printf("%-2zu - x=%-2" TREE_TYPE_SP " sum=%-2" TREE_TYPE_SP " h=%-2zu d=%-2zu b=%-2hhd -", ind, cur->x, cur->sum, cur->h, cur->deep, cur->b);
 
     if(cur->p)
     	printf(" p=%-2" TREE_TYPE_SP "", cur->p->x);
@@ -322,12 +351,12 @@ void AVL::full_print_rq(Node *cur, size_t &ind)
     printf("\n");
 }
 
-void AVL::full_print_stream_rq(Node *cur, size_t &ind, std::ostream &out)
+void AVL::print_stream_rq(Node *cur, size_t &ind, std::ostream &out)
 {
 	if(cur == nullptr) return;
-	full_print_stream_rq(cur->l, ind, out);
+	print_stream_rq(cur->l, ind, out);
 	size_t ind_left = ind;
-	full_print_stream_rq(cur->r, ind, out);
+	print_stream_rq(cur->r, ind, out);
 	size_t ind_right = ind;
     
     ind++;
@@ -351,20 +380,22 @@ void AVL::swap_childs(Node *cur)
 	cur->l = buf;
 }
 
-void AVL::restore_parent(Node *cur)
+void AVL::restore_childs(Node *cur)
 {
-	if(cur->r) cur->r->p = cur;
-	if(cur->l) cur->l->p = cur;
+	if(cur->r) {cur->r->p = cur; cur->r->deep = cur->deep + 1;}
+	if(cur->l) {cur->l->p = cur; cur->l->deep = cur->deep + 1;}
 }
 
 void AVL::balancing_node(Node* cur)
 {
-	size_t deep_l = 0, deep_r = 0;
-	if(cur->l) deep_l = cur->l->deep;
-	if(cur->r) deep_r = cur->r->deep;	
+	size_t h_l = 0, h_r = 0;
+	T sum_l = 0, sum_r = 0;
+	if(Node *cd = cur->l) {h_l = cd->h; sum_l = cd->sum + cd->x;}
+	if(Node *cd = cur->r) {h_r = cd->h; sum_r = cd->sum + cd->x;}
 
-	cur->deep = MAX(deep_l, deep_r) + 1;
-	cur->b = (signed char)(deep_l - deep_r);
+	cur->h = MAX(h_l, h_r) + 1;
+	cur->b = (signed char)(h_l - h_r);
+	cur->sum = sum_l + sum_r;
 }
 
 void AVL::small_left_rotation(Node *cur)
@@ -380,8 +411,8 @@ void AVL::small_left_rotation(Node *cur)
 	x->r = y->l;
 	y->l = buf;
 
-	restore_parent(y);
-	restore_parent(x);
+	restore_childs(x);
+	restore_childs(y);
 
 	balancing_node(y);
 	balancing_node(x);
@@ -398,9 +429,9 @@ void AVL::large_left_rotation(Node *cur)
 	x->l = z;
 	swap_childs(z);
 
-	restore_parent(z);
-	restore_parent(y);	
-	restore_parent(x);
+	restore_childs(x);
+	restore_childs(z);
+	restore_childs(y);	
 
 	balancing_node(z);
 	balancing_node(y);
@@ -419,8 +450,8 @@ void AVL::small_right_rotation(Node *cur)
 	x->l = y->r;
 	y->r = buf;
 
-	restore_parent(y);
-	restore_parent(x);
+	restore_childs(x);
+	restore_childs(y);
 
 	balancing_node(y);
 	balancing_node(x);
@@ -438,9 +469,9 @@ void AVL::large_right_rotation(Node *cur)
 	x->r = z;
 	swap_childs(z);
 
-	restore_parent(z);
-	restore_parent(y);	
-	restore_parent(x);
+	restore_childs(x);
+	restore_childs(z);
+	restore_childs(y);	
 
 	balancing_node(z);
 	balancing_node(y);
@@ -463,22 +494,6 @@ void AVL::balancing_tree(Node *cur_root)
 	else if (cur_root->b > 2 || cur_root->b < -2) {printf("Error balance, b = %d\n", cur_root->b);}
 }
 
-typedef unsigned long int hash_type;
-
-static constexpr hash_type hash_str(const char str[])
-{
-    hash_type result=0;
-	for(size_t i=0, m=1; str[i]!='\0'; i++, m*=131)
-		result += (str[i]*m);
-	return result;
-}
-
-constexpr hash_type insert_hash =  hash_str("insert");
-constexpr hash_type delete_hash =  hash_str("delete");
-constexpr hash_type exists_hash =  hash_str("exists");
-constexpr hash_type next_hash   =  hash_str("next");
-constexpr hash_type prev_hash   =  hash_str("prev");
-
 int main()
 {	
 #ifdef TEST
@@ -487,67 +502,48 @@ int main()
 
 	AVL tree;
 
-	for(char op[7]; scanf("%6s", op) != EOF;)
+	size_t n;
+	scanf("%zu\n", &n);
+
+	char prev_op = '+';
+	char prev_res;
+
+	while(n--)
 	{
-		switch(hash_str(op))
+		char op;
+		scanf("\n%c", &op);
+		switch(op)
 		{
-			case insert_hash:
+			case '+':
 			{
-				//printf("insert\n");
                 TREE_TYPE x;
-                scanf("%u", &x);
-                tree.insert(x);
+                scanf("%" TREE_TYPE_SP, &x);
+
+                if(prev_op == '+')
+            	    tree.insert(x);
+            	else
+            		tree.insert((x + prev_res)%1000000000);
+
+            	prev_op = '+';
 				break;
 			}
-			case delete_hash:
+			case '?':
 			{
-				//printf("delete\n");
-                TREE_TYPE x;
-                scanf("%u", &x);
-                tree.del(x);
-				break;
-			}
-			case exists_hash:
-			{
-				//printf("exists\n");
-                TREE_TYPE x;
-                scanf("%u", &x);
-                printf("%s\n", tree.exists(x) ? "true" : "false");
-				break;
-			}
-			case next_hash:
-			{
-				//printf("next\n")'
-                TREE_TYPE x;
-                scanf("%u", &x);
-                TREE_TYPE res;
-                if(tree.next(x, &res))
-                	printf("%" TREE_TYPE_SP "\n", res);
-                else
-                	printf("none\n");
-				break;
-			}
-			case prev_hash:
-			{
-				//printf("prev\n")'
-                TREE_TYPE x;
-                scanf("%u", &x);
-                TREE_TYPE res;
-                if(tree.prev(x, &res))
-                	printf("%" TREE_TYPE_SP "\n", res);
-                else
-                	printf("none\n");
+				TREE_TYPE l, r;
+				scanf("%" TREE_TYPE_SP " %" TREE_TYPE_SP, &l, &r);
+
+				prev_res = tree.sum(l, r);
+                printf("%" TREE_TYPE_SP "\n", prev_res);
+                prev_op = '?';
 				break;
 			}
 			default:
 			{
-                if(op[0] == '^' && op[1] == 'Z')
-                    return 0;
-				printf("Error: unknown command - \"%s\"!\n", op);
+				printf("Error: unknown command - \'%c\'!\n", op);
 				return -1;
 			}
 		}
-		tree.full_print();	
+		//tree.full_print();	
 	}
 	return 0;
 }
@@ -555,23 +551,23 @@ int main()
 
 /* 
  * quick start
-cd 2sem_AlgoLab_2\C\build
+cd 2sem_AlgoLab_2\F\build
 ..\..\run_cmake\run
 
  * set 
 mkdir build & cd build & cmake -G "MinGW Makefiles" ..
 
 * build and run
-cmake --build . && (echo START & C)
+cmake --build . && (echo START & F)
 echo return code: %errorlevel%
 
 * or
 ..\..\run_cmake\run
 
 * move
-cd 2sem_AlgoLab_2\C\build
-cd 2sem_AlgoLab_2\C
-cd C\build
+cd 2sem_AlgoLab_2\F\build
+cd 2sem_AlgoLab_2\F
+cd F\build
 
 * update run_cmake
 git submodule update --remote
